c2fb2ba6576d223d6410be5c2d783451987601be
TommyKacirek <101295400+TommyKacirek@users.noreply.github.com>
2023-03-07 14:19:45 +0100

    Update Main.java

--- src/main/java/Tree/Main.java
+++ src/main/java/Tree/Main.java
@@ -5,26 +5,69 @@ public class Main {
         Tree tree = new Tree();
 
         // Vložení prvků do stromu
-        tree.insert(1);
-        tree.insert(2);
-        tree.insert(4);
-        tree.insert(5);
         tree.insert(7);
-        tree.insert(8);
-        tree.insert(9);
-
+        
+        tree.insert(7);
+        
+        
+        tree.insert(5);
+        
+        
+        tree.insert(14);
+        
+        
+        tree.insert(1);
+        
+        
+        tree.insert(6);
+       
+        
+        tree.insert(16);
+       
+        
+        tree.insert(10);
+        
+        
+        
         // Vypsání prvky ve stromu pomocí inorder průchodu
-        System.out.print("Inorder traversal: ");
+        System.out.println("Inorder traversal: ");
         tree.inorder();
+        
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.println("Preorder traversal: ");
+        tree.preorder();
+        
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.println("Postorder traversal: ");
+        tree.postorder();
+        
+        
+        System.out.println("");
+        System.out.println("Leaves print is");
+        tree.printleaves();
+        
+        System.out.println("");
+       
 
         // Smazání uzlu a vypsání prvky pomocí inorder průchodu
         System.out.println("\nDelete node 10");
         tree.delete(10);
+        System.out.println("");
         System.out.print("Inorder traversal after deleting node 10: ");
         tree.inorder();
-
+        
+        System.out.println("");
+        
         // Vyhledání uzlu ve stromu
         System.out.println("\nSearch node 6: " + tree.search(1));
+        
+        System.out.println("");
+        
         System.out.println("Search node 10: " + tree.search(10));
+            
     }
 }
